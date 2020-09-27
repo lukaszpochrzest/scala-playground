96354fb68c79d0391e52cef54ea7fefccab5efce
Łukasz Pochrzęst <luke.pochrzest@gmail.com>
2020-09-27 16:39:51 +0200

    [MinAbsSum] better result string representation

--- src/main/scala/dynamic/Main.scala
+++ src/main/scala/dynamic/Main.scala
@@ -2,21 +2,21 @@ package dynamic
 
 object Main extends App {
 
-  class Result(val minAbsSum: Int, val signs: List[Boolean]) {
-    override def toString: String = s"Result($minAbsSum, $signs)"
+  class Result(val minAbsSum: Int, val signs: List[Int]) {
+    override def toString: String = s"Result((${signs.mkString(")+(")})=$minAbsSum)"
   }
 
   def minAbsSum(a: Seq[Int]): Result = minAbsSumRec(0, a, Nil)
 
-  private def minAbsSumRec(minAbsSum: Int, a: Seq[Int], signs: List[Boolean]): Result = a match {
+  private def minAbsSumRec(minAbsSum: Int, a: Seq[Int], signs: List[Int]): Result = a match {
     case Nil => new Result(minAbsSum, signs)
     case _ =>
-      val withPlus = minAbsSumRec(minAbsSum + a.head, a.tail, true :: signs)
-      val withMinus = minAbsSumRec(minAbsSum - a.head, a.tail, false :: signs)
+      val withPlus = minAbsSumRec(minAbsSum + a.head, a.tail, a.head :: signs)
+      val withMinus = minAbsSumRec(minAbsSum - a.head, a.tail, -a.head :: signs)
       if (Math.abs(withPlus.minAbsSum) < Math.abs(withMinus.minAbsSum)) withPlus else withMinus
   }
 
-  println(minAbsSum(Array(1, -5, 2, 2, 11)))
+  println(minAbsSum(Array(1, -5, 2, 5, 7)))
 }
 
 
